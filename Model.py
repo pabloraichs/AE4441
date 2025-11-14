5d048dcdddfb4702491697fe804691c1b19e5324
Eize Boonstra <eizekboonstra@gmail.com>
2025-11-14 14:59:43 +0100

    Add Arc class implementation and update adjacency list creation

--- Model.py
+++ Model.py
@@ -17,6 +17,17 @@ class Node:
         self.li=li # distance
         self.ti=ti #travel time
 
+class Arc:
+    def __init__(self, sigma, station, tjo, tid, i, j):
+        self.i = i
+        self.j = j
+        self.sigma = sigma
+        self.station = station
+        if tjo-tid>=self.sigma:
+            self.tij = tjo-tid
+        else:
+            self.tij = tjo-tid+24
+
 V = []
 V.append(Node(1,2,6,8,600,2))
 V.append(Node(2,1,6,8,600,2))
@@ -25,4 +36,10 @@ V.append(Node(3,1,17,21,800,4))
 V.append(Node(2,3,14,16,600,1))
 V.append(Node(3,2,19,21,600,1))
 
-print(V)
+A = []
+for i in range(len(V)):
+    for j in range(len(V)):
+        if V[i].sid == V[j].sio:
+            A.append(Arc(1.5, V[i].sid, V[j].tio, V[i].tid, i, j))
+
+print(A[8].i, A[8].j, A[8].tij)
